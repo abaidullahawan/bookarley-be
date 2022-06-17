f935b9f6689b0d063aec6fefc688ded5c29dd3a3
abbasanwar158 <abbas.anwar@devbox.co>
2022-06-17 18:34:32 +0500

    swager set params and status enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  enum status: {
+    active: 'active',
+    passive: 'passive',
+    deleted: 'deleted'
+  }, _prefix: true
+
   ransacker :extra_fields do |parent|
     Arel::Nodes::InfixOperation.new('||',
       parent.table[:extra_fields], Arel::Nodes.build_quoted(' ')
