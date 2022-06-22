a5f43d7ebb5a627418d84776c5d16152b4ef4ce2
Salman <77707310+salman245@users.noreply.github.com>
2022-06-22 17:12:17 +0500

    Revert "Added bugsnag gem."

--- config/initializers/bugsnag.rb
+++
@@ -1,8 +0,0 @@
-#frozen_string_literal: true
-
-#Bugsnag for all environments except localhost
-if Rails.env.production?
-  Bugsnag.configure do |config|
-    config.api_key = "6b758edc3597ab20156d19c36815c0ce"
-  end
-end
