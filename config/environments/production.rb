c348cb90c99eea5d293f6d7a71d785b3102d3880
Salman Tahir <salmantahir3249@gmail.com>
2022-07-19 12:13:12 +0500

    Added email confirmation for sign up.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "https://portal.tractoronline.com.pk" }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  25,
+  authentication:       'plain',
+  user_name:            'tractoronline0012@gmail.com',
+  password:             'yvmoiovmgcmozppr',
+  domain:               'gmail.com',
+ }
+
+ config.action_mailer.default_options = {
+  from: 'tractoronline0012@gmail.com'
+}
 end
