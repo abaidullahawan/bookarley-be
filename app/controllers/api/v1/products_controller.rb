ce3ed8386030ea59aa428250571e7ff1f148d3e8
Salman Tahir <salmantahir3249@gmail.com>
2022-08-02 16:50:01 +0500

    Removed authentication from products show api.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     # Brand api controller
     class ProductsController < ApplicationController
-      before_action :authenticate_api_v1_user!, except: %i[get_products]
+      before_action :authenticate_api_v1_user!, except: %i[get_products show]
       before_action :set_product, only: %i[show edit update destroy]
       require 'tempfile'
       require 'csv'
