014ccd2656baae9327010ebc9a4203a1af7b64dc
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-07-18 17:07:12 +0500

    personal add on login

--- app/controllers/api/v1/devise/sessions_controller.rb
+++ app/controllers/api/v1/devise/sessions_controller.rb
@@ -105,6 +105,7 @@ module Api
         def render_create_success
           render json: {
             data: resource_data(resource_json: @resource.token_validation_response),
+            personal_detail: @resource.personal_detail,
             role: @resource.roles
           }
         end
