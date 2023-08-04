ce6b658a2a55f1361ebc0b23b34c307ce7d0e654
AbubakarBhatti2022 <abubakarbhatti2022@gmail.com>
2023-08-04 20:30:14 +0500

    bookarley solidus

---
+++ db/migrate/20230804143538_add_logo_to_stores.solidus_multi_domain.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# This migration comes from solidus_multi_domain (originally 20130308084714)
+class AddLogoToStores < SolidusSupport::Migration[4.2]
+  def self.up
+    change_table :spree_stores do |t|
+      t.string :logo_file_name
+    end
+  end
+
+  def self.down
+    change_table :spree_stores do |t|
+      t.remove :logo_file_name
+    end
+  end
+end
