89ff844eea40ac9fe8104f6bdadacb11a4afb414
abubakar <96278949+AbubakarBhatti2022@users.noreply.github.com>
2023-09-05 20:15:43 +0500

    seller-info validation modify seller details page

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -3,6 +3,8 @@
 class UserRegistrationsController < Devise::RegistrationsController
   before_action :check_permissions, only: [:edit, :update]
   skip_before_action :require_no_authentication
+  before_action :redirect_logged_in_users, only: [:new]
+
 
   def create
     build_resource(spree_user_params)
@@ -33,5 +35,12 @@ class UserRegistrationsController < Devise::RegistrationsController
 
   def spree_user_params
     params.require(:spree_user).permit(Spree::PermittedAttributes.user_attributes | [:email])
+  end
+
+  def redirect_logged_in_users
+    if spree_current_user.present?
+      flash[:notice] = "You are already signed in."
+      redirect_to root_path
+    end
   end
 end
