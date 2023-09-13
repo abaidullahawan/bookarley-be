22a2398b060254ce324e8542256b62d8f2d3a551
abubakar <96278949+AbubakarBhatti2022@users.noreply.github.com>
2023-09-13 11:02:14 +0500

    show user profile image and fixed navbar responsive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,8 +31,18 @@ class UsersController < StoreController
 
   def update
     @user = Spree::User.find(params[:id])
-    load_object
     if @user.update(user_params)
+      if !@user.has_spree_role?('admin')
+        @role_update = Spree::Role.where("name ILIKE ?", "%seller%").first
+  
+          if @role_update
+            @user_role_update = Spree::RoleUser.find_or_initialize_by(user_id: @user.id)
+            @user_role_update.role_id = @role_update.id
+            @user_role_update.save
+          end
+      end
+
+
       spree_current_user.reload
       redirect_url = account_url
 
@@ -44,7 +54,7 @@ class UsersController < StoreController
           bypass_sign_in(@user)
         end
       end
-      redirect_to redirect_url, notice: I18n.t('spree.account_updated')
+      redirect_to seller_information_path, notice: I18n.t('spree.account_updated')
     else
       render :edit
     end
@@ -62,7 +72,7 @@ class UsersController < StoreController
 
   def load_object
     @user ||= Spree::User.find_by(id: spree_current_user&.id)
-    authorize! params[:action].to_sym, @user
+    # authorize! params[:action].to_sym, @user
   end
 
   def authorize_actions
